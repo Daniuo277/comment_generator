164e3dc75ea948af0a3e26140e8f880fa3bed442
Daniuo277 <daniel.klim277@gmail.com>
2022-07-23 17:27:14 +0200

    commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import react from react;
+
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' />
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Sam
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 6:00PM</span>
+            <div className='text'>Nice blog post!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
