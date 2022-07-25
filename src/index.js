1fd4b413f0e8c8d61f4d09abb2208f0ed8c91762
Daniuo277 <daniel.klim277@gmail.com>
2022-07-25 14:55:04 +0200

    commit_250722

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CommentDetail from './CommentDetail';
 import { randAvatar, randLine } from '@ngneat/falso';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
@@ -9,24 +10,32 @@ const root = ReactDOM.createRoot(el);
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4:45PM'
-        author_avatar={randAvatar()}
-        comment_text={randLine({ lineCount: 1 })}
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 2:00AM'
-        author_avatar={randAvatar()}
-        comment_text={randLine({ lineCount: 1 })}
-      />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 5:00PM'
-        author_avatar={randAvatar()}
-        comment_text={randLine({ lineCount: 1 })}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:45PM'
+          author_avatar={randAvatar()}
+          comment_text={randLine({ lineCount: 1 })}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='Today at 2:00AM'
+          author_avatar={randAvatar()}
+          comment_text={randLine({ lineCount: 1 })}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='Yesterday at 5:00PM'
+          author_avatar={randAvatar()}
+          comment_text={randLine({ lineCount: 1 })}
+        />
+      </ApprovalCard>
     </div>
   );
 };
